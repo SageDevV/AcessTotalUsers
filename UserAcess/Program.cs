add9d7e5de890375b48df9faa5bef4d9ca5af3c8
Pedro André <pandredbz@gmail.com>
2021-09-24 11:33:35 -0300

    Projeto finalizado, feito para retornar o numéro total de usuários que entraram em uma respectiva data em um sistema

---
+++ UserAcess/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using UserAcess.Entities;
+using System.Collections.Generic;
+
+namespace UserAcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+            
+            Console.WriteLine("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instant });
+                        Console.WriteLine(line);
+                    }
+
+                    Console.WriteLine("Total Users: " + set.Count);
+                }
+
+            }
+
+            catch (IOException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
